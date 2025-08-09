7b27e704f08796e330a9fa97d73b3b8c60e254aa
Annete <achiengomondi02@gmail.com>
2025-08-09 23:20:59 +0300

    crud operations

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView)
-from .views import AuthorViewSet, BookViewSet
+from .views import (
+    BookListView, BookDetailView, BookCreateView,
+    BookUpdateView, BookDeleteView,
+    AuthorViewSet, BookViewSet
+)
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet, basename='author')
@@ -12,7 +15,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete', BookDeleteView.as_view(), name='book-delete'),
 ]
 

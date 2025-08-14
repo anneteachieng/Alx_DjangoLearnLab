edc006b65acaca0db0b8d843db977803a757cd4e
anneteachieng <achiengomondi02@gmail.com>
2025-08-14 17:31:08 +0300

    tags

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse
 from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = TaggableManager()
+    tags = TaggableManager()  # ✅ Tagging is set up
 
     def __str__(self):
         return self.title
@@ -27,41 +27,14 @@ class Comment(models.Model):
         return f"Comment by {self.author.username} on {self.post.title}"
 
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('post-detail', kwargs={'pk': self.post.pk})
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)  # requires MEDIA setup if you want uploads
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
-
-class CommentCreateView(CreateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['pk']  # post ID from URL
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
-
-class CommentUpdateView(UpdateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
-
-    def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
-
-class CommentDeleteView(DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-
-    def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
 

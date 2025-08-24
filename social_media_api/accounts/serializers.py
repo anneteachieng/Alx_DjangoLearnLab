78739b53c236802ecaad2a831b62b9a5296ce8fc
Annete <achiengomondi02@gmail.com>
2025-08-24 16:45:09 +0300

    updating posts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
+
+
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField()
     class Meta:

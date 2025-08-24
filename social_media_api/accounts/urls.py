dbd4c8c10fb1fbc6b53f0628ec78acc8f151c6b7
Annete <achiengomondi02@gmail.com>
2025-08-24 15:40:37 +0300

    correcting urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
 

c98188aae6e8cf1466cbddc84ea603fbac45ae4c
Annete <achiengomondi02@gmail.com>
2025-08-24 16:49:36 +0300

    updating posts in social media url

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # Include accounts URLs
+    path('accounts/', include('accounts.urls')),
+    path('api/posts/', include('posts.urls')),
 ]
 
